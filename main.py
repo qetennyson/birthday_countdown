4f1d6a8749c8d09713158b1ec098b064c713cacb
qetennyson <quincytennyson8@gmail.com>
2019-01-14 19:08:20 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+def print_header():
+    pass
+
+def get_user_birthday():
+    pass
+
+def compute_days_between_dates():
+    pass
+
+def print_bday_info():
+    pass
+
+def main():
+    print_header()
+    bday = get_user_birthday()
+    now = None
+    num_days = compute_days_between_dates(bday, now)
+    print_bday_info(num_days)
+
+
