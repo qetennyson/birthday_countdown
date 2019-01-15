c9c6e4ce049d8955e8cc3e58e27b7a733a510784
qetennyson <quincytennyson8@gmail.com>
2019-01-14 19:15:10 -0500

    completing get_birthday and calling main

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
+import datetime
+
 def print_header():
-    pass
+    print('--------------------------------------------------')
+    print(' CONGRATULATIONS ON ANOTHER YEAR OF LIFE HUMAN')
+    print('--------------------------------------------------')
 
 def get_user_birthday():
-    pass
+    print("When were you born? ")
+    year = int(input("Year [YYYY]: "))
+    month = int(input("Month [MM]: "))
+    day = int(input("Day [DD]: "))
+
+    # We're trying to compare days, not moments in time, but we can do that too :)
+    birthday = datetime.date(year, month, day)
+    return birthday
 
 def compute_days_between_dates():
     pass
@@ -16,5 +27,7 @@ def main():
     now = None
     num_days = compute_days_between_dates(bday, now)
     print_bday_info(num_days)
+
+main()
 
 
